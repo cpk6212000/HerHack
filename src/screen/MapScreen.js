905b41c5e663f483e9f3da00e64d3166620bd046
cpk6212000 <cpk6212000@gmail.com>
2019-12-13 08:12:50 +0800

    Added geolocation service.

---
+++ src/screen/MapScreen.js
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+import {View, StyleSheet} from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import Geolocation from 'react-native-geolocation-service';
+const styles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+   flex: 1,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+});
+
+export default class MapScreen extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      userLatitude : 0,
+      userLongtude : 0
+    }
+  }
+  componentDidMount= () => {
+    this.locationId = Geolocation.watchPosition(
+      (position) => {
+          this.setState({
+            userLatitude: position.coords.latitude,
+            userLongtude: position.coords.longitude
+          })
+      },
+      (error) => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  );
+  }
+  componentWillUnmount= () => {
+    Geolocation.clearWatch(this.locationId)
+  }
+  render(){
+    return(<View style={styles.container}>
+      <MapView
+        followsUserLocation
+        showsUserLocation
+        provider={MapView.PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: this.state.userLatitude,
+          longitude: this.state.userLongtude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+      </MapView>
+    </View>)
+  }
+};
